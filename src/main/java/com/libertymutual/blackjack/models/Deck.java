776dc0a3836e8d3d1ee80cf6b471b9e906e066f2
Matt Fujita <matthew.fujita@libertymutual.com>
2017-08-18 11:48:15 -0700

    created draw card method i think

--- src/main/java/com/libertymutual/blackjack/models/Deck.java
+++ src/main/java/com/libertymutual/blackjack/models/Deck.java
@@ -8,29 +8,31 @@ public class Deck {
 	String[] suit = { "clubs", "spades", "diamonds", "hearts" };
 	String[] rank = { "ACE", "2", "3", "4", "5", "6", "7", "8", "9", "10", "JACK", "QUEEN", "KING" };
 	// private Card[] deck;
-	// private int cardsLeft;
+	private int cardsLeft;
 	// Shuffle method
 	// drawSingleCard
 	// dealInitialcards
 
 	// Constructor
 	public Deck() {
-		// use a list instead - do list.add
 		deck = new ArrayList<Card>();
+		//cardsLeft = deck.size() - 1;
+		cardsLeft = 52 - 1;
+
+		createDeck();
+		System.out.println(deck);
+		shuffle();
+		System.out.println(deck);
+		drawCard();
+		System.out.println(deck);
+	}
+	
+	public void createDeck() {
 		for (String s : suit)
 			for (String r : rank)
 				deck.add(new Card(r, s));
-		//System.out.println(Arrays.toString(deck));
-		System.out.println(deck);
-		
-		//printThis();
-		shuffle();
 	}
 	
-//	ArrayList<Card> deckToShuffle = new ArrayList<Card>();
-//	need random number, set variable equal to size of deck, for loop and within loop use random variable 
-//	random.nextInt(variable.size() - 1) + 1)
-//	add the generated random number to deckToShuffle
 	public void shuffle() {
 		Random random = new Random();
 		for (int i = deck.size() - 1; i >= 0; i--) {
@@ -44,16 +46,14 @@ public class Deck {
 //		System.out.print(deck);
 	}
 	
-//	public Card drawCard() {
-//		
-//	}
-
-
-//	public void printThis() {
-//		for (Card c : deck) {
-//			System.out.println(c);
-//		}
-//	}
+	public Card drawCard() {	
+		//draw new card and assign to drawnCard
+		//remove card from deck
+		Card drawnCard = deck.get(cardsLeft);
+		deck.remove(cardsLeft);
+		cardsLeft--;
+		return drawnCard;
+	}
 
 	/*
 	 * maybe a simpler way to do it (or understand) deck = new Card[52]; int index =
@@ -65,16 +65,8 @@ public class Deck {
 	 */
 	// cardsUsed = 0;
 
-	// Create an unshuffled deck of cards.
-
-	// create a stack of card objects
-	// loop to create four iterations of loop below for suits
-	// loop to create 13 cards 1-13 value
-
 	// }
 	// interogate card - 2 methods - whats the value, what's the suit?
-	// create the deck
-	// Draw a card
 
 	// Methods
 
