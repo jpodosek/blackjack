1a4a52edff743e03fe7bffa02e23b500da176237
Matt Fujita <matthew.fujita@libertymutual.com>
2017-08-20 14:21:35 -0700

    some more changes to ace handling

--- src/main/java/com/libertymutual/blackjack/models/Hand.java
+++ src/main/java/com/libertymutual/blackjack/models/Hand.java
@@ -8,7 +8,7 @@ public class Hand {
 	Hand userHand;
 	Hand dealerHand;
 	Card card;
-	private int cardRank;
+	int handScore;
 
 	public Hand() {
 		hand = new ArrayList<Card>();
@@ -28,26 +28,50 @@ public class Hand {
 
 
 	public int getHandScore() {
-		//this.hand = hand;
-		int handScore = 0;
+		handScore = 0;
 		for (Card c: hand) {
-			handScore += c.getCardValue();
-			//a way to handle the ace????
-//			if(handScore > 21) {
-//				aceCheck();
-//			}
+			if(handScore <= 21) {
+				handScore += c.getCardValue();
+			} else if(handScore > 21) {
+				//checking to see if there's an ace
+				if(aceCheck()) {
+					changeAceValue();
+					handScore += c.getCardValue();
+				}
+			}
 		} return handScore;
 	}
 	
-	
-	public int aceCheck() {
-		for(Card card : hand) {
-			if(card.getCardValue() == 11) {
-				card.setCardRank(getHandScore() - 10);
-			}
-		} 
-		return getHandScore();
+	public void changeAceValue() {
+		int sizeOfHand = hand.size();
+		for(int i = 0; i < sizeOfHand; i += 1) {
+			card = hand.remove(i); //removing the card w/ value of 11
+			Card temp = card; //storing the card into temp 
+//			int newAceValue = 1;
+			temp.setCardRank(1); //replacing 11 with value of 1
+			hand.add(temp); //adding temp card w/ value 1 back to hand
+		}
 	}
+//		for(Card card : hand) {
+//			//traverse the length of the hand, find a card w/ initial value of 11
+//			if(card.getCardValue() == 11) {
+//				//card.setCardRank(getHandScore() - 10);
+//			}
+//		} 
+//		return handScore;
+	//}
+	
+	public boolean aceCheck() {
+		boolean ace = false;
+		int currentHand = hand.size();
+		for(int i = 0; i < currentHand; i += 1) {
+			card = hand.get(i);
+			if(card.getCardValue() == 11) {
+				ace = true;
+			}
+		} return ace;
+	}
+		
 //	public int aceCheck() {
 //			//check to see if hand has an ace, if yes then equals 1, else bust
 //			ArrayList<Card> cardsInHand = userHand.getCards();
@@ -64,8 +88,4 @@ public class Hand {
 	//in game, create who instances of Hand object - one for player, one for dealer
 	//handTotalScore
 		//method add card;
-
-	public void setCardValue(int cardRank) {
-		this.cardRank = cardRank;
-	}
 }
