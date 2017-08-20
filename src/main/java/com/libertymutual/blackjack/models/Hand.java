95e53f8d31b56131f9a06e170c2d0d1bdc1c5b1a
Matt Fujita <matthew.fujita@libertymutual.com>
2017-08-20 09:46:50 -0700

    still working on ace

--- src/main/java/com/libertymutual/blackjack/models/Hand.java
+++ src/main/java/com/libertymutual/blackjack/models/Hand.java
@@ -33,13 +33,27 @@ public class Hand {
 		for (Card c: hand) {
 			handScore += c.getCardValue();
 			//a way to handle the ace????
-			if(handScore > 21 && c.getCardValue() == 11) {
-				handScore -= 10;
-			}
+//			if(handScore > 21) {
+//				for(Card card : hand) {
+//					if(card.getCardValue() == 11) {
+//						card.setCardRank(handScore - 10);
+//					}
+//				}
+//			}
 		}
 	return handScore;
 	}
 	
+	
+//	public int aceCheck() {
+//		if(getHandScore() > 21) {
+//			for(Card card : hand) {
+//				if(card.getCardValue() == 11) {
+//					card.setCardRank(getHandScore() - 10);
+//				}
+//			}
+//		} return getHandScore();
+//	}
 //	public int aceCheck() {
 //			//check to see if hand has an ace, if yes then equals 1, else bust
 //			ArrayList<Card> cardsInHand = userHand.getCards();
@@ -57,7 +71,7 @@ public class Hand {
 	//handTotalScore
 		//method add card;
 
-	public void setCardRank(int cardRank) {
+	public void setCardValue(int cardRank) {
 		this.cardRank = cardRank;
 	}
 }
